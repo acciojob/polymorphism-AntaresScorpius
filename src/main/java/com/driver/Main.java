6ee7670db5d8f92fd7f3af94d4ea61d779869617
AntaresScorpius <rohitc171@gamil.com>
2022-10-08 17:26:09 +0530

    Function Overloadedd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(20.0, 10.0);
+    }
+}
+class Product{
+    int product(int x, int y){
+        return x + y;
+    }
+    int product(int x, int y, int z){
+        return x + y + z;
+    }
+    double product(double x, double y){
+        return x + y;
+    }
 
 }
